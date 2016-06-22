bddc7dae391f637cfe7f845f4372195bc6b6a68f
BeckReed <beckreednote@outlook.com>
2016-06-22 18:02:35 +0800

    no message

--- js/extend-study.js
+++ js/extend-study.js
@@ -4,7 +4,7 @@
 
 //js继承
 
-//1.借用原型链进行继承
+//1.借用原型链进行继承 (由于存在缺点，实践中很少单独使用原型链)
 function SuperTypePrototype(){
     this.property=true;
 }
@@ -20,8 +20,110 @@ function SubTypePrototype(){
 //继承了SuperType
 SubTypePrototype.prototype=new SuperTypePrototype();
 
+//添加子类新方法、重写超类方法一定要放在以上替换原型对象之后
+//添加新方法
 SubTypePrototype.prototype.getSubValue=function(){
     return this.subproperty;
 }
+//重写超类方法
+SubTypePrototype.prototype.getSuperValue=function(){
+    return false;
+}
 
 var instanceProtoType=new SubTypePrototype();
+
+/*
+ 原型链继承方式存在的问题:
+    <1>引用类型一旦实例修改了，由于共享的原因会导致其他实例也发生改变
+    <2>没有在不影响其他对象实例的情况下给超类的构造参数传递参数
+  例子如下：
+* */
+function SuperType1(){
+    this.colors=["blue","yellow","black"];
+}
+function SubType1(){
+
+}
+
+//继承SuperType
+SubType1.prototype=new SuperType1();
+
+var instance1=new SubType1();
+instance1.colors.push("green");
+var instance2=new SubType1();//这里colors同样会受instance1的影响增加一个 同时不能传参
+
+
+//2 借用构造函数 (函数都需要写在构造函数里面，公共函数复用低 比较少单独使用这种模式继承)
+function SuperType2(){
+    this.colors=["blue","yellow","black"];
+}
+function SubType2(){
+    //继承了 SuperType
+    SuperType2.call(this);
+}
+
+
+var instance3=new SubType2();
+instance3.colors.push("green");
+var instance4=new SubType2();
+
+/*
+    借用构造函数的优点：可以传参
+    例子如下:
+* */
+function SuperTypeConstruct(name){
+    this.name=name;
+}
+function SubTypeConstruct(){
+    //继承了 SuperTypeConstruct 还传递了参数
+    SuperTypeConstruct.call(this,"秦始皇");
+    //实例属性 (为保证子类自定义同名属性不被父类的覆盖，实例属性防止在继承之后)
+    this.age=888;
+    this.des="大秦帝国第一任皇帝";
+}
+var instance5=new SubTypeConstruct();
+//以上借用构造函数实现继承存在 方法都在构造函数中定义，函数复用就无从谈起了，考虑到这些，实践中也是很少单独使用此类模式
+
+//4 原型式继承
+function createObject(o){
+    function F(){}
+    F.prototype=o;
+    return new F();
+}
+
+var person1={name:"Beck",friends:["Shelby","Court","Val"]};
+var anotherPerson1=createObject(person1);//可使用 Object.create()方法代替 但是只支持IE9+ Firefox 4+ Safari 5+ Opera 12+ Chrome
+anotherPerson1.name="Lily";
+anotherPerson1.friends.push("Tom");
+
+var anotherPerson2=createObject(person1);
+anotherPerson2.name="Lucy";
+anotherPerson2.friends.push("Cbs");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
