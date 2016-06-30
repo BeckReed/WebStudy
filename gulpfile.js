0f3c87598bb25cdbf595086bf06286dbad5d7c79
BeckReed <beckreednote@outlook.com>
2016-06-30 17:58:15 +0800

    Add build/js directory to ignore

--- gulpfile.js
+++ gulpfile.js
@@ -34,6 +34,13 @@ gulp.task('scripts',function(){
         .pipe(rename('all-lib.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('build/js/'));
+    /*多个的情况下*/
+    gulp.src('js/lib/*.js')
+        .pipe(concat('all-lib22.js'))
+        .pipe(gulp.dest('build/js/'))
+        .pipe(rename('all-lib11.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js/'));
 });
 
 
@@ -46,6 +53,18 @@ gulp.task('default',function(){
         gulp.run('lint','sass','scripts');
     });
 });
+
+/*
+         gulp的使用流程一般是这样子的：首先通过gulp.src()方法获取到我们想要处理的文件流，然后把文件流通过pipe方法导入到gulp的插件中，
+         最后把经过插件处理后的流再通过pipe方法导入到gulp.dest()中，gulp.dest()方法则把流中的内容写入到文件中，这里首先需要弄清楚的一点是，
+         我们给gulp.dest()传入的路径参数，只能用来指定要生成的文件的目录，而不能指定生成文件的文件名，它生成文件的文件名使用的是导入到它的文件流自身的文件名，
+         所以生成的文件名是由导入到它的文件流决定的，即使我们给它传入一个带有文件名的路径参数，然后它也会把这个文件名当做是目录名，例如：
+
+         var gulp = require('gulp');
+         gulp.src('script/jquery.js')
+         .pipe(gulp.dest('dist/foo.js'));
+         //最终生成的文件路径为 dist/foo.js/jquery.js,而不是dist/foo.js
+* */
 
 
 
